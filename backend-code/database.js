24e62b9801045e50276244631e78b7cbbe8416f5
EC2 Default User <ec2-user@ip-172-31-16-46.us-west-2.compute.internal>
2023-04-24 08:10:25 +0000

    setting up code deploy app

--- backend-code/database.js
+++ backend-code/database.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var conn = mysql.createConnection({
-  host: 'car-india-app.cbsacbp4qbu7.us-west-2.rds.amazonaws.com', // Replace with your host name
+  host: 'db-tier-rdsdbinstance-n7oeguiikord.cbsacbp4qbu7.us-west-2.rds.amazonaws.com', // Replace with your host name
   user: 'root',      // Replace with your database username
   password: 'Indiancar$12345',      // Replace with your database password
   database: 'cardb' // // Replace with your database Name
---
+++ scripts/restart_service.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /opt/backend-code/
+nohup npm start &
