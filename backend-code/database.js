c86ab8964ed2ee81b7a1a3fb01e7527f051c72b5
lashing <lashin_21@yahoo.com>
2023-05-07 15:37:02 +0530

    Update database.js

--- backend-code/database.js
+++ backend-code/database.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var conn = mysql.createConnection({
-  host: 'db-tier-rdsdbinstance-7pncgonf2t6h.cbsacbp4qbu7.us-west-2.rds.amazonaws.com', // Replace with your host name
+  host: 'rds-tier-rdsdbinstance-i6wlows4ibg4.cbsacbp4qbu7.us-west-2.rds.amazonaws.com', // Replace with your host name
   user: 'root',      // Replace with your database username
   password: 'Indiancar$12345',      // Replace with your database password
   database: 'cardb' // // Replace with your database Name
